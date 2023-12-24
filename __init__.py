9bd19bf8e884fcb8859e72659fdda00b2a77312d
danil09234 <dailzag05@gmail.com>
2023-12-24 02:29:10 +0200

    Add module exceptions

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from .get_page import get_page_with_json_render
 from .config import set_api_key
+from .exceptions import InitializationError
 
 
-__all__ = ["get_page_with_json_render", "set_api_key"]
+__all__ = ["get_page_with_json_render", "set_api_key", "InitializationError"]
