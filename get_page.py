da9a0d099644e1863c78b8b4b8c365e595bd3802
danil09234 <dailzag05@gmail.com>
2024-01-27 20:04:24 +0200

    Add retries

--- get_page.py
+++ get_page.py
@@ -4,7 +4,7 @@ from .exceptions import InitializationError
 from loguru import logger
 
 
-async def get_page_with_json_render(url: str, api_key: str | None = None) -> str:
+async def get_page_with_json_render(url: str, api_key: str | None = None, retries: int = 2) -> str:
     logger.info("Getting page with json render...")
     if api_key is None:
         client = get_client()
@@ -14,6 +14,18 @@ async def get_page_with_json_render(url: str, api_key: str | None = None) -> str
         client = ZenRowsClient(api_key)
     logger.success("ZenRows client was selected")
     logger.info("Awaiting response from ZenRows...")
-    response = await client.get_async(url, params={"js_render": "true", "block_resources": "image,media,font"})
+
+    while response := await client.get_async(url, params={"js_render": "true", "block_resources": "image,media,font"}):
+        if response.status_code == 200:
+            break
+
+        logger.warning(f"ZenRows returned invalid status code {response.status_code}. Response is {response.text}")
+
+        if retries <= 0:
+            break
+
+        retries -= 1
+        logger.info("Retrying...")
+
     logger.success("Response from ZenRows received. Returning...")
     return response.text
