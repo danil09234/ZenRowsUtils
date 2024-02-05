2170fc419c7509840feac072b35d5ec89e6ca7f8
danil09234 <dailzag05@gmail.com>
2024-02-06 01:41:05 +0200

    Add `get_page_with_custom_headers` function

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-from .get_page import get_page_with_json_render
+from .get_page import get_page_with_json_render, get_page_with_custom_headers
 from .config import set_api_key
 from .exceptions import InitializationError
 
 
-__all__ = ["get_page_with_json_render", "set_api_key", "InitializationError"]
+__all__ = ["get_page_with_json_render", "get_page_with_custom_headers", "set_api_key", "InitializationError"]
--- get_page.py
+++ get_page.py
@@ -1,22 +1,22 @@
+from requests import Response
 from zenrows import ZenRowsClient
 from .config import get_client
 from .exceptions import InitializationError, OutOfQuotaException
 from loguru import logger
 
 
-async def get_page_with_json_render(url: str, api_key: str | None = None, retries: int = 2) -> str:
-    logger.info("Getting page with json render...")
+def get_client_helper(api_key: str | None) -> ZenRowsClient:
     if api_key is None:
         client = get_client()
         if client is None:
             raise InitializationError("No API key for the request")
     else:
         client = ZenRowsClient(api_key)
-    logger.success("ZenRows client was selected")
-    logger.info("Awaiting response from ZenRows...")
+    return client
 
-    while ((response := await client.get_async(url, params={"js_render": "true", "block_resources": "image,media,font"}))
-           or True):
+
+async def retries_helper(client: ZenRowsClient, retries: int, url: str, *args, **kwargs) -> Response:
+    while (response := await client.get_async(url, *args, **kwargs)) or True:
         if response.status_code == 200:
             break
 
@@ -31,6 +31,31 @@ async def get_page_with_json_render(url: str, api_key: str | None = None, retrie
 
         retries -= 1
         logger.info("Retrying...")
+    return response
+
+
+async def get_page_with_json_render(url: str, api_key: str | None = None, retries: int = 2) -> str:
+    logger.info("Getting page with json render...")
+    client = get_client_helper(api_key)
+    logger.success("ZenRows client was selected")
+    logger.info("Awaiting response from ZenRows...")
+
+    response = await retries_helper(
+        client, retries, url,
+        params={"js_render": "true", "block_resources": "image,media,font"}
+    )
+
+    logger.success(f"Response from ZenRows received (status code {response.status_code}). Returning...")
+    return response.text
+
+
+async def get_page_with_custom_headers(url: str, headers: dict, api_key: str | None = None, retries: int = 2) -> str:
+    logger.info("Getting page with custom headers render...")
+    client = get_client_helper(api_key)
+    logger.success("ZenRows client was selected")
+    logger.info("Awaiting response from ZenRows...")
+
+    response = await retries_helper(client, retries, url, headers=headers)
 
     logger.success(f"Response from ZenRows received (status code {response.status_code}). Returning...")
     return response.text
